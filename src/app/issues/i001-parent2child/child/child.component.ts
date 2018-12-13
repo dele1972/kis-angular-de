cf7eaccb80b700f51d709761a7f9ba9c0dc1871f
Dennis Lederich <dennis@lederich.de>
2018-12-13 13:29:18 +0100

    kleine Änderungen am Wording (Templates und readme)

--- src/app/issues/i001-parent2child/child/child.component.ts
+++ src/app/issues/i001-parent2child/child/child.component.ts
@@ -4,22 +4,21 @@ import { Component, Input } from '@angular/core';
   selector: 'app-child',
   template: `
   <div class="chip bg-dark">Child</div>
-    <p>
-      Parent sagt: "{{childMessage}}"
+    <p class="text-secondary">
+    Parent sagt: <strong class="text-error">"{{childMessage}}"</strong>
     </p>
-    <ul>
+    <hr />
+    <h5>ToDo (Child)</h5>
+    <ol>
         <li>
-          Eine <a href="https://angular.io/guide/template-syntax#input-and-output-properties">Input Property</a> 
-          wird mit dem <code>@Input</code> Dekorator erstellt: <code>@Input() childMessage: string;</code>
+          Input Dekorator importieren:<br /> 
+          <code>import {{ '{' }} ..., Input {{ '}' }} from '@angular/core';</code>
         </li>
         <li>
-          Damit wird eine Eigenschaft der Kompenente hinzugefügt, welche das Parent durch Property Binding binden kann.
+          Element- und Komponenten-Eigenschaften definieren und binden:<br />
+          <code>@Input() childMessage: string;</code>
         </li>
-        <li>
-          Im Child kann der Propertyname quasi als Variable weiter benutzt werden - z. B. um per String Interpolation den 
-          Wert im Child-Template auszugeben: <code>{{childMessage}}</code>
-        </li>
-    </ul>
+    </ol>
   `,
   styles: []
 })
